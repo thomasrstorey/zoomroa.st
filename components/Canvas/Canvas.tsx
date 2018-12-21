796f21ff47c0123880dd0158a46504ce4517a59b
Thomas R Storey <thomas@feathr.co>
2018-12-21 12:34:57 -0500

    wip: poc for selector controls

--- components/Canvas/Canvas.tsx
+++ components/Canvas/Canvas.tsx
@@ -1,12 +1,14 @@
-import { KonvaEventObject } from 'konva';
+import Konva from 'konva';
 import React from 'react';
-import { Image as CanvasImage, Layer, Rect, Stage } from 'react-konva';
+import { Group, Image as CanvasImage, KonvaContainerComponent, Layer, Rect, Stage } from 'react-konva';
+
+type TGroup = KonvaContainerComponent<Konva.Group<Konva.Node>, Konva.ContainerConfig>;
 
 interface IProps {
   imageFile: File;
 }
 
-const onWheel = (event: KonvaEventObject<WheelEvent>) => {
+const onWheel = (event: Konva.KonvaEventObject<WheelEvent>) => {
   event.evt.preventDefault();
   const scaleBy = 1.03;
   const stage = event.target.getStage();
@@ -27,7 +29,9 @@ const onWheel = (event: KonvaEventObject<WheelEvent>) => {
 };
 
 const Canvas = (props: IProps) => {
-  let imageNode: CanvasImage|null = null;
+  let imageNode: Konva.Image|null = null;
+  let group: Konva.Group<Konva.Node>|null = null;
+  let layer: Konva.Layer|null;
   const image = new Image();
   image.onload = () => {
     if (imageNode) {
@@ -42,9 +46,66 @@ const Canvas = (props: IProps) => {
       draggable={true}
       onWheel={onWheel}
     >
-      <Layer>
-        <CanvasImage image={image} ref={(node: CanvasImage) => { imageNode = node; }} />
-        <Rect x={450} y={300} width={200} height={200} stroke="yellow" strokeEnabled={true} strokeWidth={5} draggable={true} />
+      <Layer ref={(node: Konva.Layer) => { layer = node; }}>
+        <CanvasImage image={image} ref={(node: Konva.Image) => { imageNode = node; }} />
+        <Group x={450} y={300} ref={(node) => { group = node; }} draggable={true}>
+          <Rect
+            x={0}
+            y={0}
+            fill="yellow"
+            fillEnabled={true}
+            width={20}
+            height={20}
+            offset={{ x: 10, y: 10 }}
+            draggable={true}
+            onMouseDown={(event: Konva.KonvaEventObject<MouseEvent>) => {
+              if (group) {
+                group.draggable(false);
+              }
+            }}
+            onDragMove={(event: Konva.KonvaEventObject<DragEvent>) => {
+
+            }}
+            onDragEnd={() => {
+              if (group) {
+                group.draggable(true);
+              }
+              if (layer) {
+                layer.draw();
+              }
+            }}
+            onMouseOver={(event: Konva.KonvaEventObject<MouseEvent>) => {
+              document.body.style.cursor = 'pointer';
+              const rect = event.currentTarget as Konva.Rect;
+              rect.moveToTop();
+              rect.width(22);
+              rect.height(22);
+              rect.offset({x: 11, y: 11 });
+              if (layer) {
+                layer.draw();
+              }
+            }}
+            onMouseOut={(event: Konva.KonvaEventObject<MouseEvent>) => {
+              document.body.style.cursor = 'default';
+              const rect = event.currentTarget as Konva.Rect;
+              rect.width(20);
+              rect.height(20);
+              rect.offset({x: 10, y: 10 });
+              if (layer) {
+                layer.draw();
+              }
+            }}
+          />
+          <Rect
+            x={0}
+            y={0}
+            width={200}
+            height={200}
+            stroke="yellow"
+            strokeEnabled={true}
+            strokeWidth={5}
+          />
+        </Group>
       </Layer>
     </Stage>
   );
